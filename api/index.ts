0cf95b090aace8ecbe8a5231c7b412cb52dde664
antunes145 <antunes145@gmail.com>
2026-04-11 15:06:48 -0400

    Fix Vercel API module resolution

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { Express } from "express";
-import { createApp } from "../server/app";
+import { createApp } from "../server/app.ts";
 
 let appPromise: Promise<Express> | null = null;
 
